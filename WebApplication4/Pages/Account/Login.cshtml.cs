8e3d740697e263cc93c13c28ef858a37e0611763
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-10-26 18:15:59 +0300

    adding login and logout

--- WebApplication4/Pages/Account/Login.cshtml.cs
+++ WebApplication4/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace WebApplication4.Pages.Account
 {
@@ -13,14 +15,35 @@ namespace WebApplication4.Pages.Account
         {
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid) { return Page(); }
+            //Verify the Credential
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                //Creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,"admin@mywebsite.com"),
+                    new Claim("Department", "HR"),
+                    new Claim("Admin", "true"),
+                    new Claim("Manager", "true")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
         }
 
         public class LoginCredential
         {
             [Required]
+            [Display(Name ="User Name")]
             public string UserName { get; set; }
             [Required]
             [DataType(DataType.Password)]
